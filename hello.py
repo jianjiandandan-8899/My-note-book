73744b7e26e1275e59643643bddb6c317d497b10
jianjiandandan-8899 <133863375+jianjiandandan-8899@users.noreply.github.com>
2026-05-28 15:51:16 +0100

    Initial commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+
+name = "Alice"
+age = 30
+
+
+print(f"hello, {name}!")
+
+def greet(person):
+    return f"hello,{person}"
+
+message = greet("Bob")
+print(message)
+
+def add(a,b):
+    
+    return a + b
+
+add(5,3)
+
+
